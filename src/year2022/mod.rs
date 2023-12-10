347eae7545f00f8a1347e026f4fe336c853b712f
Andy Innes <artificialintelligencea@gmail.com>
2023-12-10 01:34:58 +0000

    Day 6, still 2022

--- src/year2022/mod.rs
+++ src/year2022/mod.rs
@@ -3,6 +3,7 @@ mod day_2;
 mod day_3;
 mod day_4;
 mod day_5;
+mod day_6;
 
 pub fn runner(day: &String) -> String {
     if day == "1" {
@@ -15,6 +16,8 @@ pub fn runner(day: &String) -> String {
         format!("{}", day_4::solution())
     } else if day == "5" {
         format!("{}", day_5::solution())
+    } else if day == "6" {
+        format!("{}", day_6::solution())
     } else {
         format!("Unknown day input {day}")
     }

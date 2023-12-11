f90a650dedddc61c7ab3fd2972e52afd51599e11
Andy Innes <artificialintelligencea@gmail.com>
2023-12-11 20:23:27 +0000

    Day 11, part 2 feat overflows

--- src/year2023/day_11/mod.rs
+++ src/year2023/day_11/mod.rs
@@ -1,79 +1,92 @@
 use std::collections::HashSet;
 use itertools::Itertools;
 
+#[derive(Clone, Copy, Eq, PartialEq)]
 enum SpaceType {
     Galaxy(i32),
     Empty,
     Expanded,
 }
 
-fn parse(contents: &str) -> Vec<Vec<Option<i32>>> {
-    let mut output: Vec<Vec<Option<i32>>> = vec![];
+fn parse(contents: &str) -> Vec<Vec<SpaceType>> {
+    let mut output: Vec<Vec<SpaceType>> = vec![];
     let mut galaxy_count = 0;
     let base_col_count = contents.lines().next().unwrap().len();
     let mut columns_without_galaxy: HashSet<usize> = HashSet::from_iter(0..base_col_count);
     for line in contents.lines() {
-        let mut row: Vec<Option<i32>> = vec![];
+        let mut row: Vec<SpaceType> = vec![];
         let mut null_row = true;
         for (col, c) in line.chars().enumerate() {
             if c == '.' {
-                row.push(None);
+                row.push(SpaceType::Empty);
             } else {
                 null_row = false;
-                row.push(Some(galaxy_count));
+                row.push(SpaceType::Galaxy(galaxy_count));
                 galaxy_count += 1;
                 columns_without_galaxy.remove(&col);
             }
         }
-        output.push(row.clone());
         if null_row {
+            output.push(vec![SpaceType::Expanded; row.len()]);
+        } else {
             output.push(row);
         }
     }
     if columns_without_galaxy.len() > 0 {
         for col in columns_without_galaxy.iter().sorted().rev() {
             for row in output.iter_mut() {
-                row.insert(*col, None);
+                row[*col] = SpaceType::Expanded;
             }
         }
     }
     output
 }
 
-fn galaxies(map: &Vec<Vec<Option<i32>>>) -> Vec<(usize, usize)> {
+fn galaxies(map: &Vec<Vec<SpaceType>>) -> Vec<(usize, usize)> {
     let mut output = vec![];
     for (r, row) in map.iter().enumerate() {
         for (c, col) in row.iter().enumerate() {
-            if *col != None {
-                output.push((r, c));
-            }
+            match col {
+                SpaceType::Galaxy(_) => output.push((r, c)),
+                _ => continue
+            };
         }
     }
     output
 }
 
-fn path_length(a: &(usize, usize), b: &(usize, usize)) -> usize {
+fn path_length(map: &Vec<Vec<SpaceType>>, expansion: i128, a: &(usize, usize), b: &(usize, usize)) -> i128 {
     let mut dist = 0;
-    if a.0 > b.0 {
-        dist += a.0 - b.0;
-    } else {
-        dist += b.0 - a.0;
+    let start_row = a.0.min(b.0);
+    let end_row = a.0.max(b.0);
+    let start_col = a.1.min(b.1);
+    let end_col = a.1.max(b.1);
+    for row_num in start_row..end_row {
+        let current = map[row_num][start_col];
+        if current == SpaceType::Expanded {
+            dist += expansion;
+        } else {
+            dist += 1;
+        }
     }
-    if a.1 > b.1 {
-        dist += a.1 - b.1;
-    } else {
-        dist += b.1 - a.1;
+    for col_num in start_col..end_col {
+        let current = map[start_row][col_num];
+        if current == SpaceType::Expanded {
+            dist += expansion;
+        } else {
+            dist += 1;
+        }
     }
     dist
 }
 
-fn problem_1(galaxy_list: &Vec<(usize, usize)>) -> usize {
+fn problem_solve(map: &Vec<Vec<SpaceType>>, galaxy_list: &Vec<(usize, usize)>, expansion: i128) -> i128 {
     let mut result = 0;
     for i in 0..galaxy_list.len() - 1 {
         let left = galaxy_list[i];
         for j in i + 1..galaxy_list.len() {
             let right = galaxy_list[j];
-            result += path_length(&left, &right);
+            result += path_length(map, expansion, &left, &right);
         }
     }
     result
@@ -81,7 +94,11 @@ fn problem_1(galaxy_list: &Vec<(usize, usize)>) -> usize {
 
 pub fn solution() -> String {
     let contents = include_str!("input.txt");
-    let readings = parse(contents);
-    let galaxy_list = galaxies(&readings);
-    format!("Problem 1: {}\nProblem 2: {}", problem_1(&galaxy_list), 2)
+    let map = parse(contents);
+    let galaxy_list = galaxies(&map);
+    format!(
+        "Problem 1: {}\nProblem 2: {}",
+        problem_solve(&map, &galaxy_list, 2),
+        problem_solve(&map, &galaxy_list, 1000000),
+    )
 }
